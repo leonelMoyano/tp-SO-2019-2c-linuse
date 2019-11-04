1df552618f93ce8db5947959ef929666e0185dd3
papa-caco <ccomesa@gmail.com>
2019-11-04 12:22:21 -0300

    -

--- hilolay-example/Debug/src/subdir.mk
+++ hilolay-example/Debug/src/subdir.mk
@@ -4,23 +4,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/suseDefs.c \
-../src/suseServer.c 
+../src/example_alumnos.c \
+../src/example_program.c 
 
 OBJS += \
-./src/suseDefs.o \
-./src/suseServer.o 
+./src/example_alumnos.o \
+./src/example_program.o 
 
 C_DEPS += \
-./src/suseDefs.d \
-./src/suseServer.d 
+./src/example_alumnos.d \
+./src/example_program.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -include"/home/utnso/workspace/tp-2019-2c-No-C-Nada/hilolay/bin/libhilolay.so" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
