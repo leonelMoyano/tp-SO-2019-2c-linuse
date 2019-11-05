e9e0c2419d60bac96e4e2500640456faaeafc3bd
leonelMoyano <leonel.alejandro.moyano@gmail.com>
2019-11-04 21:29:32 -0300

    Fix bug de ejemplo de FUSE

--- FUSE/src/sac_cli.c
+++ FUSE/src/sac_cli.c
@@ -61,12 +61,14 @@ static int do_getattr(const char *path, struct stat *st) {
 	if (strcmp(path, "/") == 0) {
 		st->st_mode = S_IFDIR | 0755;
 		st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
-	} else if ( (strcmp(path, "/file349") == 0 )  || ( strcmp(path, "/file54") == 0 )){
+	} else if ( (strcmp(path, "/file349") == 0 ) || ( strcmp(path, "/file54") == 0 )){
 		st->st_mode = S_IFREG | 0644;
 		st->st_nlink = 1;
 		st->st_size = 1024;
+	} else {
+		return -ENOENT;
 	}
-	return -ENOENT;
+	return 0;
 }
 
 static int do_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
