187be8f808e0238e4fa4d621ba5467f45468b180
leonelMoyano <leonel.alejandro.moyano@gmail.com>
2019-12-20 20:58:01 -0300

    MUSE: freno el pagefault cuando encuentro victime

--- MUSE/src/manejoEstructuras.c
+++ MUSE/src/manejoEstructuras.c
@@ -388,7 +388,9 @@ int ClockModificado() {
 		} else if (aux->flagPresencia == true && aux->flagUso == false ) {
 			paginaVictima = aux;
 			paginaVictima->flagPresencia = false;
+			log_debug( g_logger, "Faulteo pag %d de frame %d", paginaVictima->nroPagina, paginaVictima->nroFrame );
 			cargarFrameASwap(paginaGlobal->nroFrame, paginaGlobal);
+			break;
 		}
 	}
 
